57d91e710aced9df83e653d5733e36b68775264a
Vivek Kumar Bansal <vkb0310@gmail.com>
2019-04-29 22:36:40 +0530

    update emotion styles

--- examples/styled-datepicker/src/Components.tsx
+++ examples/styled-datepicker/src/Components.tsx
@@ -1,15 +1,12 @@
-// import * as React from 'react';
 import { DayOfMonth } from 'react-date-primitives';
-import styled from 'react-emotion';
-import { CSSObject } from 'create-emotion';
-
+import styled from '@emotion/styled';
 /**
  * These are internal modules to `react-date-primitives` and are used only for demo purposes only.
  * Please do not use in production. This might break in future without any notice.
  *
  * Use your favourite date library (eg: moment, date-fns, etc.) instead.
  */
-import { isSameDay } from 'react-date-primitives/esm/components/utils';
+import { isSameDay } from 'react-date-primitives/esm/utils';
 
 const bgColor = '#d9534f';
 const bgColorHover = '#eee';
@@ -18,156 +15,140 @@ const colorHover = color;
 const colorSelected = '#fff';
 const colorOutsideMonth = '#ddd';
 
-export const Wrapper = styled('div')({
-    display: 'inline-flex',
-    border: '1px solid #eee'
-});
+export const Wrapper = styled.div`
+    display: inline-flex;
+    border: 1px solid #eee;
+`;
 
-export const LHS = styled('div')({
-    background: bgColor,
-    padding: '12px',
-    width: '300px',
-    textAlign: 'center',
-    display: 'grid',
-    gridTemplateColumns: '100%',
-    gridTemplateRows: '25% 35% auto',
-    justifyItems: 'center',
-    alignItems: 'center',
-    lineHeight: 1
-});
+export const LHS = styled.div`
+    background: ${bgColor};
+    padding: 12px;
+    width: 300px;
+    text-align: center;
+    display: grid;
+    grid-template-columns: 100%;
+    grid-template-rows: 25% 35% auto;
+    justify-items: center;
+    align-items: center;
+    line-height: 1;
+`;
 
-export const LHSDay = styled('div')({
-    color: '#fff'
-});
+export const LHSDay = styled.div`
+    color: #fff;
+`;
 
-export const LHSDate = styled('div')({
-    fontSize: '4.5em',
-    fontWeight: 'bold',
-    color: '#fff'
-});
+export const LHSDate = styled.div`
+    font-size: 4.5em;
+    font-weight: bold;
+    color: #fff;
+`;
 
-export const LHSMonthAndYear = styled('div')({
-    fontSize: '24px',
-    opacity: 0.3,
-    textTransform: 'uppercase',
-    alignSelf: 'start'
-});
+export const LHSMonthAndYear = styled.div`
+    font-size: 24px;
+    opacity: 0.6;
+    text-transform: uppercase;
+    align-self: start;
+    color: #fff;
+`;
 
-export const RHS = styled('div')({
-    margin: '12px',
-    textAlign: 'center',
-    display: 'grid',
-    gridTemplateColumns: 'repeat(7, 38px)',
-    gridTemplateRows: 'repeat(8, 38px)',
-    justifyItems: 'center',
-    lineHeight: '38px',
-    alignItems: 'center'
-});
+export const RHS = styled.div`
+    margin: 12px;
+    text-align: center;
+    display: grid;
+    grid-template-columns: repeat(7, 38px);
+    grid-template-rows: repeat(8, 38px);
+    justify-items: center;
+    line-height: 38px;
+    align-items: center;
+`;
 
-export const RHSMonthName = styled('div')({
-    fontWeight: 'bold',
-    textAlign: 'left',
-    justifySelf: 'left',
-    paddingLeft: '10px',
-    gridColumnStart: 1,
-    gridColumnEnd: 6
-});
+export const RHSMonthName = styled.div`
+    font-weight: bold;
+    text-align: left;
+    justify-self: left;
+    padding-left: 10px;
+    grid-column: 1 / 6;
+`;
 
 export interface ButtonProps {
     direction: 'left' | 'right';
 }
 
-export const Button = styled('button')(
-    {
-        width: '28px',
-        height: '28px',
-        display: 'block',
-        position: 'relative',
-        cursor: 'pointer',
-        border: 'none',
-        fontWeight: 'bold',
-        backgroundColor: 'transparent',
-        borderRadius: '4px',
-        transition: 'background-color 0.1s ease-in-out',
-        overflow: 'hidden',
-        outline: 'none',
-        '&:hover': {
-            backgroundColor: bgColorHover
-        },
-        '&::before': {
-            content: '""',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            height: '100%',
-            width: '100%',
-            display: 'block',
-            backgroundImage: 'url(./chevron.svg)',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: '50%',
-            backgroundPosition: '50%'
-        }
-    },
-    (props: ButtonProps) => ({
-        '&::before': {
-            transform: props.direction === 'right' ? 'rotate(-90deg)' : 'rotate(90deg)'
-        }
-    })
-);
+export const Button = styled.button<ButtonProps>`
+    width: 28px;
+    height: 28px;
+    display: block;
+    position: relative;
+    cursor: pointer;
+    border: none;
+    font-weight: bold;
+    background-color: transparent;
+    border-radius: 4px;
+    transition: background-color 0.1s ease-in-out;
+    overflow: hidden;
+    outline: none;
+    line-height: 1;
 
-const cellCSS: CSSObject = {
-    width: '100%',
-    height: '100%',
-    color
-};
+    &:hover {
+        background-color: ${bgColorHover}
+    }
+}`;
 
-export const Cell = styled('div')(cellCSS);
+export const Cell = styled.div`
+    width: 100%;
+    height: 100%;
+    color: ${color};
+`;
 
 const today = new Date();
 
-export const Day = styled('div')(cellCSS, (args: DayOfMonth) => {
-    const isToday = isSameDay(today, args.date);
+export const Day = styled.div<DayOfMonth & { selected: boolean }>`
+    width: 100%;
+    height: 100%;
+    color: ${color};
+    border-radius: 4px;
+    transition: background-color 0.1s ease-in-out;
+    cursor: pointer;
+    font-weight: ${(props) => (isSameDay(today, props.date) || props.selected ? 'bold' : 'normal')};
+    background: ${(props) => (props.selected ? bgColor : 'transparent')};
+    color: ${(props) => {
+        const isToday = isSameDay(today, props.date);
 
-    return {
-        borderRadius: '4px',
-        fontWeight: isToday || args.selected ? 'bold' : 'normal',
-        transition: 'background-color 0.1s ease-in-out',
-        background: args.selected ? bgColor : 'transparent',
-        color: (() => {
-            if (!args.inCurrentMonth) {
+        if (!props.inCurrentMonth) {
+            return colorOutsideMonth;
+        }
+
+        if (isToday && !props.selected) return bgColor;
+
+        if (props.selected) return colorSelected;
+
+        return color;
+    }};
+
+    &:hover {
+        color: ${(props) => {
+            const isToday = isSameDay(today, props.date);
+
+            if (!props.inCurrentMonth) {
                 return colorOutsideMonth;
             }
 
-            if (isToday && !args.selected) return bgColor;
+            if (isToday && !props.selected) return bgColor;
 
-            if (args.selected) return colorSelected;
+            if (props.selected) return colorSelected;
 
-            return color;
-        })(),
-        cursor: 'pointer',
-        '&:hover': {
-            color: (() => {
-                if (!args.inCurrentMonth) {
-                    return colorOutsideMonth;
-                }
+            return colorHover;
+        }};
+        background: ${(props) => {
+            if (!props.inCurrentMonth) {
+                return 'transparent';
+            }
 
-                if (isToday && !args.selected) return bgColor;
+            if (props.selected) {
+                return bgColor;
+            }
 
-                if (args.selected) return colorSelected;
-
-                return colorHover;
-            })(),
-            background: (() => {
-                if (!args.inCurrentMonth) {
-                    return 'transparent';
-                }
-
-                if (args.selected) {
-                    return bgColor;
-                }
-
-                return bgColorHover;
-            })()
-        }
-    };
-});
+            return bgColorHover;
+        }};
+    }
+`;
--- examples/styled-datepicker/src/DatePicker.tsx
+++ examples/styled-datepicker/src/DatePicker.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
-import { CalendarMonth, DayOfMonth, CalendarMonthRenderProps } from 'react-date-primitives';
-
+import { useCalendar, DayOfMonth } from 'react-date-primitives';
 /**
  * These are internal modules to `react-date-primitives` and are used only for demo purposes only.
  * Please do not use in production. This might break in future without any notice.
  *
  * Use your favourite date library (eg: moment, date-fns, etc.) instead.
  */
-import { addMonths } from 'react-date-primitives/esm/components/utils';
+import { addMonths, isSameDay } from 'react-date-primitives/esm/utils';
 
 import {
     Wrapper,
@@ -53,83 +52,66 @@ export interface SimpleDatePickerState {
     day: Date;
 }
 
-export class DatePicker extends React.Component<{}, SimpleDatePickerState> {
-    constructor(props: {}) {
-        super(props);
+export function DatePicker() {
+    const { month, setMonth, days } = useCalendar();
+    const [day, setDay] = React.useState(new Date());
+    const monthName = MONTH_NAMES[month.getMonth()];
 
-        this.state = {
-            month: new Date(),
-            day: new Date()
+    function handleDayClick(currentDay: DayOfMonth) {
+        return () => {
+            setDay(currentDay.date);
         };
     }
 
-    handleMonthIncrement = (e: React.MouseEvent<HTMLButtonElement>) => {
-        this.setState((state) => ({
-            month: addMonths(state.month, 1)
-        }));
-    };
+    function handleMonthIncrement() {
+        setMonth(addMonths(month, 1));
+    }
 
-    handleMonthDecrement = (e: React.MouseEvent<HTMLButtonElement>) => {
-        this.setState((state) => ({
-            month: addMonths(state.month, -1)
-        }));
-    };
+    function handleMonthDecrement() {
+        setMonth(addMonths(month, -1));
+    }
 
-    handleDayClick = (day: DayOfMonth) => () => {
-        this.setState({ day: day.date });
-    };
-
-    renderMonth = (renderProps: CalendarMonthRenderProps) => {
-        const { days } = renderProps;
-
-        return (
-            <React.Fragment>
+    return (
+        <Wrapper>
+            <LHS>
+                <LHSDay>{DAYS_LONG[day.getDay()]}</LHSDay>
+                <LHSDate>{day.getDate()}</LHSDate>
+                <LHSMonthAndYear>
+                    {MONTH_NAMES[day.getMonth()]} {day.getFullYear()}
+                </LHSMonthAndYear>
+            </LHS>
+            <RHS>
+                <RHSMonthName>
+                    {monthName} {month.getFullYear()}
+                </RHSMonthName>
+                <div>
+                    <Button direction="left" onClick={handleMonthDecrement}>
+                        &lt;
+                    </Button>
+                </div>
+                <div>
+                    <Button direction="right" onClick={handleMonthIncrement}>
+                        &gt;
+                    </Button>
+                </div>
+                {DAYS_SHORT.map((d, i) => (
+                    <Cell style={{ fontWeight: 'bold' }} key={i}>
+                        {d}
+                    </Cell>
+                ))}
                 {days.map((week, i) =>
                     week.map((d, j) => (
-                        <Day {...d} key={`${i}-${j}`} onClick={this.handleDayClick(d)}>
+                        <Day
+                            {...d}
+                            selected={isSameDay(d.date, day)}
+                            key={`${i}-${j}`}
+                            onClick={handleDayClick(d)}
+                        >
                             {d ? d.date.getDate() : ''}
                         </Day>
                     ))
                 )}
-            </React.Fragment>
-        );
-    };
-
-    render() {
-        const { month, day } = this.state;
-        const monthName = MONTH_NAMES[month.getMonth()];
-
-        return (
-            <Wrapper>
-                <LHS>
-                    <LHSDay>{DAYS_LONG[day.getDay()]}</LHSDay>
-                    <LHSDate>{day.getDate()}</LHSDate>
-                    <LHSMonthAndYear>
-                        {MONTH_NAMES[day.getMonth()]} {day.getFullYear()}
-                    </LHSMonthAndYear>
-                </LHS>
-                <RHS>
-                    <RHSMonthName>
-                        {monthName} {month.getFullYear()}
-                    </RHSMonthName>
-                    <div>
-                        <Button direction="left" onClick={this.handleMonthDecrement}>
-                            &nbsp;
-                        </Button>
-                    </div>
-                    <div>
-                        <Button direction="right" onClick={this.handleMonthIncrement}>
-                            &nbsp;
-                        </Button>
-                    </div>
-                    {DAYS_SHORT.map((d, i) => (
-                        <Cell style={{ fontWeight: 'bold' }} key={i}>
-                            {d}
-                        </Cell>
-                    ))}
-                    <CalendarMonth month={month} startDate={day} render={this.renderMonth} />
-                </RHS>
-            </Wrapper>
-        );
-    }
+            </RHS>
+        </Wrapper>
+    );
 }
